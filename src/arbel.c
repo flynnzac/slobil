3b1d63ba1e973314b69032cf4903378c68514bcc
Zach Flynn <zlflynn@gmail.com>
2020-01-30 01:48:02 -0500

    Have a version of ARBEL which can be controlled remotely.

--- src/arbel.c
+++ src/arbel.c
@@ -22,7 +22,8 @@
 #include "arbel.h"
 #include <unistd.h>
 #include <signal.h>
-
+#include <sys/socket.h>
+#include <netinet/in.h>
 int
 dummy_event ()
 {
@@ -60,9 +61,8 @@ main (int argc, char** argv)
   #ifdef GARBAGE
   GC_INIT();
   #endif
-  source_code = NULL;
   rl_event_hook = dummy_event;
-  signal(SIGINT, interrupt_handler);
+  source_code = NULL;
   registry* reg = new_registry(NULL, ARBEL_HASH_SIZE);
   add_basic_ops(reg);
   is_exit(0);
@@ -98,17 +98,19 @@ main (int argc, char** argv)
   int echo = 1;
   char* script = NULL;
   int save_code = 1;
-  while ((k = getopt(argc, argv, "l:s:nmvc:")) != -1)
+  bool listen_socket = false;
+  int port = 0;
+  while ((k = getopt(argc, argv, "l:s:nmvc:p:")) != -1)
     {
       switch (k)
         {
-	case 'c':
-	  state.in_comment = 1;
-	  is_exit(1);
-	  script = malloc(sizeof(char)*(strlen(optarg)+1));
+        case 'c':
+          state.in_comment = 1;
+          is_exit(1);
+          script = malloc(sizeof(char)*(strlen(optarg)+1));
           strcpy(script, optarg);
           save_code = 0;
-	  break;
+          break;
         case 'n':
           is_exit(1);
           break;
@@ -129,12 +131,21 @@ main (int argc, char** argv)
         case 'd':
           save_code = 0;
           break;
-	case 'v':
-	  printf("%s\n", PACKAGE_STRING);
-	  is_exit(1);
-	  break;
+        case 'p':
+          listen_socket = true;
+          port = atoi(optarg);
+          if (port <= 0)
+            {
+              fprintf(stderr, "Invalid port: %s\n", optarg);
+              is_exit(1);
+            }
+          break;
+        case 'v':
+          printf("%s\n", PACKAGE_STRING);
+          is_exit(1);
+          break;
         default:
-          fprintf(stderr, "Option not recognized.");
+          fprintf(stderr, "Option not recognized.\n");
           is_exit(1);
           break;
         }
@@ -150,19 +161,90 @@ main (int argc, char** argv)
 
   state.print_out = echo;
 
+  if (!listen_socket)
+    {
+
+      signal(SIGINT, interrupt_handler);
+    }
+
+
+  int sock;
+  struct sockaddr_in addr;  
+  if (listen_socket)
+    {
+      sock = socket(AF_INET, SOCK_STREAM, 0);
+
+      int opt = 1;
+      addr.sin_family = AF_INET;
+      addr.sin_addr.s_addr = INADDR_ANY;
+      addr.sin_port = htons(4120);
+      setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                 &opt, sizeof(opt));
+      bind(sock, (struct sockaddr*) &addr,
+           sizeof(addr));
+    }
+        
+
   while (!is_exit(-1))
     {
-      if (complete)
+      if (listen_socket)
         {
-          code = readline(prompt);
+          /* remove gc garbage collection because later code assumes readline
+             string which is stdlib malloc'd */
+
+          #ifdef GARBAGE
+          #undef realloc
+          #undef malloc
+          #endif
+
+          listen(sock, 3);
+          size_t sz_addr = sizeof(addr);
+          int new_sock = accept(sock, (struct sockaddr*) &addr,
+                                &sz_addr);
+          code = malloc(sizeof(char)*1024);
+          size_t i = 0;
+          size_t cur_size = 1024;
+          char c[2];
+          read(new_sock,&c, 1);
+          
+          while (c[0] != EOF)
+            {
+              if (i >= cur_size)
+                {
+                  code = realloc(code, sizeof(char)*(cur_size+1024));
+                  cur_size += 1024;
+                }
+              code[i] = c[0];
+              i++;
+              read(new_sock,&c, 1);
+            }
+
+          if (i >= cur_size)
+            {
+              code = realloc(code, sizeof(char)*(cur_size+1));
+            }
+          code[i] = '\0';
+          #ifdef GARBAGE
+          #define realloc(x,y) GC_REALLOC(x,y)
+          #define malloc(x) GC_MALLOC(x)
+          #endif
+
         }
       else
         {
-          code = readline("");
+          if (complete)
+            {
+              code = readline(prompt);
+            }
+          else
+            {
+              code = readline("");
+            }
+          add_history(code);
         }
-      add_history(code);
+      
       code = append_nl(code);
-
+      
       if (save_code)
         {
           source_code = append_to_source_code(source_code, code);
