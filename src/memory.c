34e4e3c76a0df0a9edc772a73ebcc4db38e62aec
Zach Flynn <zlflynn@gmail.com>
2023-06-18 15:26:03 -0500

    License update

--- src/memory.c
+++ src/memory.c
@@ -1,22 +1,16 @@
 /* 
+   SLOBIL
+   Copyright 2023 Zach Flynn
 
-   SLOBIL is a Object Based Environment and Language
-   Copyright 2021 Zach Flynn <zlflynn@gmail.com>
+   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-   This file is part of SLOBIL.
+   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
-   SLOBIL is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 
-   SLOBIL is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
-   You should have received a copy of the GNU General Public License
-   along with SLOBIL (in COPYING file).  If not, see <https://www.gnu.org/licenses/>.
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
    
 */
 
--- src/slobil.h
+++ src/slobil.h
@@ -1,21 +1,16 @@
-/* 
-   SLOBIL is a Object Based Environment and Language
-   Copyright 2021 Zach Flynn <zlflynn@gmail.com>
+/*
+  SLOBIL
+  Copyright 2023 Zach Flynn
 
-   This file is part of SLOBIL.
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-   SLOBIL is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
-   SLOBIL is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 
-   You should have received a copy of the GNU General Public License
-   along with SLOBIL (in COPYING file).  If not, see <https://www.gnu.org/licenses/>.
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
    
 */
 
@@ -54,19 +49,19 @@
  */
 enum data_type
   {
-   Integer = 1,
-   Real = 2,
-   String = 4,
-   Slot = 8,
-   Object = 16,
-   Instruction = 32,
-   Expression = 64,
-   Operation = 128,
-   File = 256,
-   Boolean = 512,
-   Nothing = 1024,
-   NotAType = 2048,
-   Task = 4096
+    Integer = 1,
+    Real = 2,
+    String = 4,
+    Slot = 8,
+    Object = 16,
+    Instruction = 32,
+    Expression = 64,
+    Operation = 128,
+    File = 256,
+    Boolean = 512,
+    Nothing = 1024,
+    NotAType = 2048,
+    Task = 4096
   };
 
 typedef enum data_type data_type;
@@ -285,10 +280,10 @@ typedef struct task task;
 
 enum print_settings
   {
-   PRINT_PLAIN = 0,
-   PRINT_ANSWER = 1,
-   PRINT_NEWLINE = 2,
-   PRINT_QUOTES = 4
+    PRINT_PLAIN = 0,
+    PRINT_ANSWER = 1,
+    PRINT_NEWLINE = 2,
+    PRINT_QUOTES = 4
   };
 
 typedef enum print_settings print_settings;
